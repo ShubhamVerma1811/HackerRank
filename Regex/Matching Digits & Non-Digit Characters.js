f61148294786ac2ae0fdbd6712e3ed2210a2334f
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-08-07 11:12:44 +0530

    sovled: regex prob

---
+++ Regex/Matching Digits & Non-Digit Characters.js
@@ -0,0 +1,16 @@
+var Regex_Pattern = /\d{2}\D\d{2}\D\d{4}/;
+
+function processData(Test_String) {
+  console.log(!!Test_String.match(Regex_Pattern));
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+_input = '';
+process.stdin.on('data', function (input) {
+  _input += input;
+});
+
+process.stdin.on('end', function () {
+  processData(_input);
+});
